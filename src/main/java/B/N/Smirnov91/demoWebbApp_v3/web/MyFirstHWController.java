e1809010108edd935d3626c446a7cca5bd00b400
Borissun <zgreen3@gmail.com>
2018-09-19 12:21:21 +0300

    commit # 3

--- src/main/java/B/N/Smirnov91/demoWebbApp_v3/web/MyFirstHWController.java
+++ src/main/java/B/N/Smirnov91/demoWebbApp_v3/web/MyFirstHWController.java
@@ -1,7 +1,6 @@
 package B.N.Smirnov91.demoWebbApp_v3.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
