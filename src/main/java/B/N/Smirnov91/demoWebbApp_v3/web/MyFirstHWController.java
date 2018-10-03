439a7f7b439518c4f0206914e166487c94911501
Boris Smirnov <zgreen3@gmail.com>
2018-10-03 23:10:14 +0300

    New commit of v3 Lab 1 with a test

--- src/main/java/B/N/Smirnov91/demoWebbApp_v3/web/MyFirstHWController.java
+++ src/main/java/B/N/Smirnov91/demoWebbApp_v3/web/MyFirstHWController.java
@@ -1,14 +1,15 @@
 package B.N.Smirnov91.demoWebbApp_v3.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyFirstHWController {
 
-    @RequestMapping("/hello")
-    public String responsePrint(@RequestParam(value = "name") String name) {
-        return "Hello " + name + "!";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String responsePrint(@RequestParam(value = "name", defaultValue = "stranger") String name) {
+        return "Hello, " + name + "!";
     }
 }
---
+++ src/test/java/B/N/Smirnov91/demoWebbApp_v3/DemoWebbAppV3ApplicationTest.java
@@ -0,0 +1,35 @@
+package B.N.Smirnov91.demoWebbApp_v3;
+
+///*
+import static org.hamcrest.Matchers.containsString;
+//import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//*/
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class DemoWebbAppV3ApplicationTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void testGetMyFirstHWControllerIndex() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, stranger!")));
+	}
+
+}
